729ebf00ae9164f69601ee49f34bad91e38c6037
isaac <isaacvinicius2121@alu.ufc.br>
2022-11-28 19:47:31 -0300

    update - formato da lista + exemplos

--- Trabalho-Final/programacao-logica/Q2.pl
+++ Trabalho-Final/programacao-logica/Q2.pl
@@ -3,8 +3,15 @@
 %Utilizando Prolog implemente um predicado n_esimo(N, L, X), onde X é o N-ésimo
 %elemento da lista L.
 
-%formato da lista: [p1, p2, p3, p4, p5].
-n_esimo(1, [X|_], X).
+%formato da lista: [p0, p1, p2, p3, p4, p5].
+n_esimo(0, [X|_], X).
 n_esimo(N, [_|Y], X):-
     n_esimo(N1, Y, X),
     N is N1+1.
+/*
+?- n_esimo(1,[2,3,4,5,6],3).
+true
+
+?- n_esimo(1,[2,3,4,5,6],7).
+false.
+*/
